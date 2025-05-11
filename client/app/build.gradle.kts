3c5be5ace31e26ab626105caa0fe00fefb3205fc
brumbuz <vvv210308@gmail.com>
2025-05-11 21:42:16 +0500

    fix build.gradle.kts and libs.versions.toml

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -2,15 +2,14 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
-    namespace = "com.example.client"
+    namespace = "com.example.myapplication"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.client"
+        applicationId = "com.example.myapplication"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -41,10 +40,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.ktor.client.logging)
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.android)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -53,7 +52,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.compose.jvmstubs)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,28 +59,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    // Compose
-    implementation (platform("androidx.compose:compose-bom:2023.08.00")) // Используйте актуальную версию
-    implementation ("androidx.compose.ui:ui")
-    implementation ("androidx.compose.ui:ui-graphics")
-    implementation ("androidx.compose.ui:ui-tooling-preview")
-    implementation ("androidx.compose.material3:material3")
-
-    // Activity & Fragment KTX для by viewModels()
-    implementation ("androidx.activity:activity-ktx:1.9.0") // Используйте актуальную версию
-    implementation ("androidx.fragment:fragment-ktx:1.7.0" )// Если используете фрагменты
-
-    // Lifecycle (ViewModel)
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0") // Используйте актуальную версию
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0") // Используйте актуальную версию
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0") // Для viewModel() в Composables
-
-    // Navigation Compose
-    implementation ("androidx.navigation:navigation-compose:2.8.0-beta01") // Используйте актуальную версию
-
-    // Ktor
-    implementation ("io.ktor:ktor-client-core:2.3.9") // Используйте актуальную версию
-    implementation ("io.ktor:ktor-client-android:2.3.9")
-    implementation ("io.ktor:ktor-client-logging:2.3.9")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.okhttp)
 }
